01ea9043d1f5782da46a857947c7cf5150f2e071
Ajay Sharma <gridking111@gmail.com>
2026-04-20 16:12:07 +0300

    Initial Commit

---
+++ RoleUp/RoleUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoleUpApp.swift
+//  RoleUp
+//
+//  Created by Work Laptop on 20/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct RoleUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RoleUpTests/RoleUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  RoleUpTests.swift
+//  RoleUpTests
+//
+//  Created by Work Laptop on 20/04/2026.
+//
+
+import Testing
+@testable import RoleUp
+
+struct RoleUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
